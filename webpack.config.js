645c631077dae5db33a8ae3f2127d7d5530ca3af
Mognek <simopp0301@gmail.com>
2025-11-20 19:46:19 +0100

    eseguito tutte le correzioni necessarie, integrato webpack e axios per le chiamate API

---
+++ webpack.config.js
@@ -0,0 +1,49 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+    clean: true
+  },
+  mode: 'development',
+  devtool: 'inline-source-map',
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader']
+      },
+      {
+        test: /\.(png|jpg|jpeg|gif|svg)$/i,
+        type: 'asset/resource',
+        generator: {
+          filename: 'assets/IMG/[name][ext]'
+        }
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './index.html',
+      filename: 'index.html',
+      favicon: './src/assets/IMG/logo.png'
+    })
+  ],
+  devServer: {
+  static: './dist',
+  port: 8080,
+  open: true,
+  proxy: [
+    {
+      context: ['/api'],
+      target: 'https://openlibrary.org',
+      changeOrigin: true,
+      secure: false,
+      pathRewrite: { '^/api': '' },
+    }
+  ]
+}
+};
